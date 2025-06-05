6a58710dff76f6a00a8db57a4817dd090b140f23
daipengin <62413457+daipengin@users.noreply.github.com>
2025-06-05 11:56:52 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref} from 'vue'
+
+const text = ref('')
+
+function onInput(e) {
+  text.value = e.target.value
+}
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <input :value="text" v-on:input="onInput" placeholder="Type here">
+  <p>{{ text }}</p>
 </template>
 
 <style scoped>
